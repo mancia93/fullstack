7ded5b897fc39db2b622db1b0cd3ef99a7a5dd50
mancia08 <teikomancini1993@gmail.com>
2021-01-27 11:11:35 +0100

    routes setup

--- BE/app.js
+++ BE/app.js
@@ -12,6 +12,9 @@ const db = require("./database/configs");
 const app = express();
 
 //route for register, login and profile
+const loginRouter = require("./routes/login");
+const registerRouter = require("./routes/register");
+const profileRouter = require("./routes/profile");
 
 //db connection
 db.connector
@@ -20,6 +23,15 @@ db.connector
   .catch(err => console.error(`DB sync failed: ${err}`));
 
 //app global middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//set the routes
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+app.use("/profile", profileRouter);
+
+//global middleware for errors
 app.use((req, res, next) => {
   let err = new Error("Not found");
   err.status = 404;
